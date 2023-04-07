6ff0e975926b00b51868ba94e147597fb0bf37f1
hibaby03 <hibaby03@snu.ac.kr>
2023-04-07 11:54:27 +0900

    calc.py

---
+++ calc.py
@@ -0,0 +1,21 @@
+def main():
+    print("Let's implement addition, multiple. Type two numbers for x and y.")
+
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    print("%d + %d = %d " % (x, y, add(x, y)))   
+    print("%d * %d = %d " % (x, y, multiple(x, y)))   
+      
+
+#daadfsfas
+# TODO: add() 함수 정의
+def add(x,y):
+    return x+y
+    
+def multiple(x,y):
+	return x*y
+
+if __name__ == "__main__":
+    main()
+    
