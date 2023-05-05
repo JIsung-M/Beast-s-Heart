a157bc2cae3c029e09f76bd0c3704cad678ca48b
jwe11197 <130066870+jwe11197@users.noreply.github.com>
2023-05-05 17:07:18 +0900

    Create skeleton1.py

---
+++ skeleton1.py
@@ -0,0 +1,57 @@
+# !/usr/bin/env python3
+import rospy
+import numpy as np
+
+from sensor_msgs.msg import Image, CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+from std_msgs.msg import Header
+
+class DetermineColor:
+    def __init__(self):
+        self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.callback)
+        self.color_pub = rospy.Publisher('/rotate_cmd', Header, queue_size=10)
+        self.bridge = CvBridge()
+
+    def callback(self, data):
+        try:
+            # listen image topic
+            image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+
+            # prepare rotate_cmd msg
+            # DO NOT DELETE THE BELOW THREE LINES!
+            msg = Header()
+            msg = data.header
+            msg.frame_id = '0'  # default: STOP
+
+            # determine background color
+            # TODO
+            # determine the color and assing +1, 0, or, -1 for frame_id
+            # msg.frame_id = '+1' # CCW (Blue background)
+            # msg.frame_id = '0'  # STOP
+            # msg.frame_id = '-1' # CW (Red background)
+            flatten_image=image.reshape(-1.3)
+            x=flatten_image.shape[0]
+            for i in range(x)
+                bins=np.bincount(flatten_image[i,0]) +np.bincount(flatten_image[i,1])+np.bincount(flatten_image[i,2])
+                background_color=np.argmax(bins)
+                if background_color==-1
+                    msg.frame_id='-1'
+                elif background_color==0
+                    msg.frame_id='0'
+                elif background_color==1
+                    msg.frame_id='+1'
+            # publish color_state
+            self.color_pub.publish(msg)
+
+        except CvBridgeError as e:
+            print(e)
+
+
+    def rospy_shutdown(self, signal, frame):
+        rospy.signal_shutdown("shut down")
+        sys.exit(0)
+
+if __name__ == '__main__':
+    detector = DetermineColor()
+    rospy.init_node('CompressedImages1', anonymous=False)
+    rospy.spin()
